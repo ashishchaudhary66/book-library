ed90de0fd3d9fd92947a2a9ee2cbfea7dbe4e61c
Ashish Kumar Chaudhary <ashish.chaudhary02@nagarro.com>
2025-10-21 11:14:52 +0530

    Book Icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Header.css'
+import icon from '../image/book.png';
 
 function Header() {
   return (
     <div className='Header'>
-        <div className='header-icon'>Book Library Icon</div>
+        <div className='header-icon'>
+          <img src={icon} alt='Library Icon' width='45px' height='35px' />
+        </div>
         <h2 className='header-text'>Book Library</h2>
     </div>
   )
